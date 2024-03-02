75e38c0362db7ccaa0ec3aab1337291854fcc5ff
Salemas <sauliusbrz@gmail.com>
2024-03-02 22:14:22 +0200

    letter count

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ def main():
     with open("./books/frankenstein.txt") as file:
         file_content = file.read()
 
-    words = file_content.split()
+    lower_case = file_content.lower()
+    words = lower_case.split()
     print(len(words))
+    print(words[0])
 
 if __name__ == "__main__":
     main()
