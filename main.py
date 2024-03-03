b16d4a18481d89995d1a8638e7e6e23199582ba6
Salemas <sauliusbrz@gmail.com>
2024-03-03 19:48:55 +0200

    finished

--- main.py
+++ main.py
@@ -11,15 +11,32 @@ def howMuchLetters(file):
 
     return letter_count
 
+def converter(dict):
+    nice_list = []
+    for key in dict:
+        if key.isalpha():
+            nice_list.append({"letter":key, "count":dict[key]})
+    nice_list.sort(reverse=True, key = lambda list: list["count"])
+    return nice_list
+
+def report(list, number):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{number} words found in the document")   
+    for word in list:
+        print(f"The '{word['letter']}' character was found {word['count']} times")
+    print("--- End report of books/frankenstein.txt ---")
+
 def main():
     with open("./books/frankenstein.txt") as file:
         file_content = file.read()
 
     lower_case = file_content.lower()
     words = lower_case.split()
-    print(len(words))
+ #   print(len(words))
     count = howMuchLetters(words)
-    print(count)
+ #   print(count)
+    
+    report(converter(count), len(words))
 
 if __name__ == "__main__":
     main()
